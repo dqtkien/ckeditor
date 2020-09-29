a7aaf608aad956188d8d2b7978e20eabf764ba46
kevin <dqtkien@gmail.com>
2020-09-29 14:10:00 +0700

    upgrade to v22.0

--- source-editor/src/ckeditor.js
+++ source-editor/src/ckeditor.js
@@ -14,7 +14,6 @@ import Italic from "@ckeditor/ckeditor5-basic-styles/src/italic";
 import BlockQuote from "@ckeditor/ckeditor5-block-quote/src/blockquote";
 import CKFinder from "@ckeditor/ckeditor5-ckfinder/src/ckfinder";
 import EasyImage from "@ckeditor/ckeditor5-easy-image/src/easyimage";
-import Font from "@ckeditor/ckeditor5-font/src/font";
 import Heading from "@ckeditor/ckeditor5-heading/src/heading";
 import Image from "@ckeditor/ckeditor5-image/src/image";
 import ImageCaption from "@ckeditor/ckeditor5-image/src/imagecaption";
@@ -23,15 +22,19 @@ import ImageToolbar from "@ckeditor/ckeditor5-image/src/imagetoolbar";
 import ImageUpload from "@ckeditor/ckeditor5-image/src/imageupload";
 import Indent from "@ckeditor/ckeditor5-indent/src/indent";
 import Link from "@ckeditor/ckeditor5-link/src/link";
-import List from "@ckeditor/ckeditor5-list/src/list";
+import ListStyle from "@ckeditor/ckeditor5-list/src/liststyle";
 import MediaEmbed from "@ckeditor/ckeditor5-media-embed/src/mediaembed";
 import Paragraph from "@ckeditor/ckeditor5-paragraph/src/paragraph";
 import PasteFromOffice from "@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice";
 import Table from "@ckeditor/ckeditor5-table/src/table";
 import TableToolbar from "@ckeditor/ckeditor5-table/src/tabletoolbar";
+import TextTransformation from "@ckeditor/ckeditor5-typing/src/texttransformation";
+
+//add in Plugin
+
+import Font from "@ckeditor/ckeditor5-font/src/font";
 import TableProperties from "@ckeditor/ckeditor5-table/src/tableproperties";
 import TableCellProperties from "@ckeditor/ckeditor5-table/src/tablecellproperties";
-import TextTransformation from "@ckeditor/ckeditor5-typing/src/texttransformation";
 import Underline from "@ckeditor/ckeditor5-basic-styles/src/underline";
 import Strikethrough from "@ckeditor/ckeditor5-basic-styles/src/strikethrough";
 import Code from "@ckeditor/ckeditor5-basic-styles/src/code";
@@ -42,6 +45,7 @@ import Highlight from "@ckeditor/ckeditor5-highlight/src/highlight";
 import ImageResize from "@ckeditor/ckeditor5-image/src/imageresize";
 import LinkImage from "@ckeditor/ckeditor5-link/src/linkimage";
 import Alignment from "@ckeditor/ckeditor5-alignment/src/alignment";
+
 export default class ClassicEditor extends ClassicEditorBase {}
 
 const customColorPalette = [
@@ -132,18 +136,11 @@ const customColorPalette = [
 // Plugins to include in the build.
 ClassicEditor.builtinPlugins = [
   Essentials,
-  Font,
   UploadAdapter,
   Autoformat,
   Bold,
   Italic,
-  Underline,
-  Strikethrough,
-  Code,
-  Subscript,
-  Superscript,
   BlockQuote,
-  Highlight,
   CKFinder,
   EasyImage,
   Heading,
@@ -154,15 +151,23 @@ ClassicEditor.builtinPlugins = [
   ImageUpload,
   Indent,
   Link,
-  List,
+  ListStyle,
   MediaEmbed,
   Paragraph,
   PasteFromOffice,
   Table,
   TableToolbar,
+  TextTransformation,
+  Font,
+  Underline,
+  Strikethrough,
+  Code,
+  Subscript,
+  Superscript,
+  BlockQuote,
+  Highlight,
   TableProperties,
   TableCellProperties,
-  TextTransformation,
   SimpleUploadAdapter,
   ImageResize,
   LinkImage,
